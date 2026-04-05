688e26271edc58df1dcfa23a4d26dff9129aa508
Amirali Rezaei <amiralirezaie169@gmail.com>
2026-04-05 11:31:39 +0200

    i made a Form , with 3 Select - and i writen 2 Functions for give age and distance.
    Ho creato un modulo con 3 menu a tendina e ho scritto 2 funzioni per età e distanza.

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,46 @@
-const chilometri = document.querySelector('#km');
+const origine = document.querySelector('#origine');
+const destinazione = document.querySelector('#destinazione');
 const eta = document.querySelector('#age');
 const button = document.querySelector('#btn');
 
+for (let i = 4; i <= 110; i++) {
+    eta.innerHTML += `<option value="${i}">${i}</option>`;
+}
+
+const distanze = {
+    "milano-roma": 570,
+    "milano-torino": 140,
+    "milano-venezia": 270,
+    "milano-napoli": 770,
+    "milano-firenze": 300,
+    "roma-torino": 670,
+    "roma-venezia": 530,
+    "roma-napoli": 230,
+    "roma-firenze": 280,
+    "torino-venezia": 400,
+    "torino-napoli": 880,
+    "torino-firenze": 390,
+    "venezia-napoli": 730,
+    "venezia-firenze": 260,
+    "napoli-firenze": 470
+}
+const calcoDistanza = () => {
+    const da = origine.value;
+    const a = destinazione.value;
+    if (da === a) {
+        return;
+    } else {
+        const part1 = `${da}-${a}`;
+        const part2 = `${a}-${da}`;
+        const distanza = distanze[part1] || distanze[part2];
+        return distanza;
+    }
+}
+
+
 const calcolaPrezzo = () => {
 
-    let prezzo = chilometri.value * 0.21;
+    let prezzo = calcoDistanza() * 0.21;
 
     if (eta.value < 18) {
         prezzo = prezzo * 0.8;   // sconto 20%
@@ -15,7 +51,10 @@ const calcolaPrezzo = () => {
     const prezzoFinale = `${prezzo.toFixed(2)}€`;
     console.log(prezzoFinale);
     return prezzoFinale;
+
 }
 
-button.addEventListener("click", calcolaPrezzo);
-
+button.addEventListener("click", (e) => {
+    e.preventDefault(); 
+    calcolaPrezzo();
+});
